257d8df0ef8a76084a4cec6531556a8ee8a4ae40
AJEETSINGHPATEL <patelajeet514@gmail.com>
2026-01-19 01:52:44 +0530

    Fix import path for api in CompanyProfile

--- pages/company/[id].js
+++ pages/company/[id].js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { api } from '../../../utils/api';
+ import api from '../../utils/api';
 import { FaStar, FaBuilding, FaMapMarkerAlt, FaLink, FaCheckCircle, FaExclamationCircle } from 'react-icons/fa';
 
 export default function CompanyProfile() {
